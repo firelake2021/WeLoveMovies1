2aea30ea5402ed3a0a16b1b5a0c3cd6186649c5b
Elena Krotkova <ekrotkova@indeed.com>
2024-01-18 15:00:52 -0600

    set database url

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 require("dotenv").config();
 
-const { DATABASE_URL = "postgresql://postgres@localhost/postgres" } =
-  process.env;
+// const { DATABASE_URL = "postgresql://postgres@localhost/postgres" } =
+//   process.env;
 // const {
 //   NODE_ENV = "development",
 //   DEVELOPMENT_DATABASE_URL,
@@ -14,6 +14,10 @@ const { DATABASE_URL = "postgresql://postgres@localhost/postgres" } =
 //   NODE_ENV === "production"
 //     ? PRODUCTION_DATABASE_URL
 //     : DEVELOPMENT_DATABASE_URL;
+
+const {
+  DATABASE_URL = "postgres://dev_ty21_user:GrPIfJOhlSfX6Sj1ZQwZhSOP9e8TCHM5@dpg-cm2r5eda73kc73elsrj0-a.oregon-postgres.render.com/dev_ty21?ssl=true",
+} = process.env;
 
 module.exports = {
   development: {
